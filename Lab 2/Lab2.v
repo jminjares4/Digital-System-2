026f723e3d8b42b2f23749a566569d36090802f2
jminjares4 <minjares.jesus.ths@gmail.com>
2022-02-10 17:32:51 -0700

    Updated lab 2

--- Lab 2/Lab2.v
+++ Lab 2/Lab2.v
@@ -1,25 +1,12 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/07/2022 10:37:26 AM
-// Design Name: 
-// Module Name: Lab2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file    Lab1.v
+ * @author  Jesus Minjares
+ * @date    2022-02-10
+ * @brief   Create a Full-adder and Hexadecimal adder using AND and OR gates
+ */
+`timescale 10ns / 1ps
 
-
+//Or module with 2 inputs
 module Or(
     input A,
     input B,
@@ -27,6 +14,7 @@ module Or(
     assign Out= A|B;
 endmodule
 
+//And module with 2 inputs
 module And(
     input A,
     input B,
@@ -34,51 +22,14 @@ module And(
     assign Out= A&B;
 endmodule
 
-
-//module Inverter(
-//    input A,
-//    output reg B);
-//    always @ (A)
-//    begin
-//        if(A == 1'b0)
-//        begin
-//             B = 1'b1;
-//        end
-//        else
-//             B = 1'b0;
-//    end       
-//endmodule
-
-//module Lab2(
-//    input A, 
-//    input B, 
-//    input Cin, 
-//    output Sum, 
-//    output Cout
-//    );    
-//endmodule
-
+//FullAdder module of two bits 
 module FullAdder(input A, input B, input C, output Sum, output Cout);
-////    wire a, b, cin,
-////    wire a0,a1,a2,a3,a4;    
-////    wire o0, o1; 
-////    And And1(.A(~A), .B(~C), .Out(a0)); //~A~C
-////    And And2(.A(~B), .B(~C), .Out(a1));//~B~C
-////    And And3(.A(A), .B(B), .Out(a2)); // AB
-////    And And4(.A(A), .B(C), .Out(a3)); // AC
-////    And And5(.A(a2),.B(C), .Out(a4)); // AB & C -> ABC
-    
-////    Or Or1(.A(a0), .B(a1), .Out(o0)); //~A~C + ~B~C
-    
-////    Or Or2(.A(a2), .B(a3), .Out(o1));  //AB + AC 
-    
-////    Or Or3(.A(o0), .B(a4), .Out(Sum)); // ~A~C + ~B~C + ABC
-    
-////    Or Or4(.A(o1), .B(~a1), .Out(Cout)); // AC + AB +BC
-    
+
+    //create wires to store and and or outputs
     wire a01,a02,a03,a04;
     wire a11,a12,a13,a14;
     wire o01, o02;
+    //instantiate AND and OR gates for the SUM
     And And1(.A(~A),.B(~B),.Out(a01)); 
     And And2(.A(~A),.B(B),.Out(a02));
     And And3(.A(A),.B(B), .Out(a03));
@@ -91,7 +42,7 @@ module FullAdder(input A, input B, input C, output Sum, output Cout);
     Or Or2(.A(a13),.B(a14),.Out(o02));
     Or Or3(.A(o01),.B(o02),.Out(Sum));
     
-    
+    // instantiate AND and OR gates for the Cout
     wire c1,c2,c3,c4;
     And And9(.A(A),.B(C),.Out(c1));
     And And10(.A(A),.B(B),.Out(c2));
@@ -100,17 +51,14 @@ module FullAdder(input A, input B, input C, output Sum, output Cout);
     Or Or5(.A(c4),.B(c3),.Out(Cout));
 endmodule
 
-
+//Hexadecimal adder with two 4 bits inputs and 4 bits sum output 
 module HexAdder(input [3:0]A, input[3:0]B, input C, output [3:0]Sum, output Cout);
+    //create wire to store Cin output
     wire co, c1, c2;
-    
+    //instantiate Fulll-adder modules
     FullAdder f1(.A(A[0]),.B(B[0]),.C(C),.Sum(Sum[0]),.Cout(co));
     FullAdder f2(.A(A[1]),.B(B[1]),.C(co),.Sum(Sum[1]),.Cout(c1));
     FullAdder f3(.A(A[2]),.B(B[2]),.C(c1),.Sum(Sum[2]),.Cout(c2));
     FullAdder f4(.A(A[3]),.B(B[3]),.C(c2),.Sum(Sum[3]),.Cout(Cout));
     
 endmodule  
-
-//input [3:0]Name
-//module HexAdder(input A[4], input B[4], input Cin[4], output Sum[4], output Cout[4]);
-//endmodule
--- Lab 2/Lab2tb.v
+++ Lab 2/Lab2tb.v
@@ -1,65 +1,35 @@
+/**
+ * @file    Lab1.v
+ * @author  Jesus Minjares
+ * @date    2022-02-10
+ * @brief   Create a testbench for Full-adder and Hexadecimal adder using AND and OR gates
+ */
+
 `timescale 10ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/07/2022 10:38:12 AM
-// Design Name: 
-// Module Name: Lab2tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module Lab2tb;
-//    reg A,B,Ci;
-//    wire Co, S;
-    
-//    FullAdder uut(
-//        .A(A),
-//        .B(B), 
-//        .C(Ci),
-//        .Sum(S),
-//        .Cout(Co)
-//     );
-    
+    //create wires and register for the inputs
     reg [3:0]A;
     reg [3:0]B;
     reg C;
     wire [3:0]S;
     wire Co; 
-    
-    
+    //instantiate HexAdder module  
     HexAdder uut(.A(A),.B(B), .C(C), .Sum(S),.Cout(Co));
+
+//start test bench
 initial
 begin
-     
-//     A = 0; B = 0; Ci = 0;
-//#1   A = 0; B = 0; Ci = 1;
-//#1   A = 0; B = 1; Ci = 0;
-//#1   A = 0; B = 1; Ci = 1;
-//#1   A = 1; B = 0; Ci = 0;
-//#1   A = 1; B = 0; Ci = 1;
-//#1   A = 1; B = 1; Ci = 0;
-//#1   A = 1; B = 1; Ci = 1;
 
-
+//loop for 10 iterations
 repeat (10)
 begin
-#10
+#10 //add same delay
+// store random number from 0-16
 A = $random() % 16;
 B = $random() % 16;
 C = $random() % 16;
 end
-
 
 #5 $finish; // ends the code
     end
